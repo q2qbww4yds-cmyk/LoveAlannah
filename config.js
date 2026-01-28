25ba3daea621a8779a65cf5c31daf8d023a65128
Chris W <q2qbww4yds@privaterelay.appleid.com>
2026-01-28 14:55:25 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Alannah Aurelis",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! I'm the luckiest person from jamaica and in the world! 🎉💝💖💝💓",
+        message: "Now come get your gift, a big warm hug,a huge kiss and BUDDY!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
